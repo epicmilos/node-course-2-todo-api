89b10bd62343a506a6316acbc2e584d5b89ca161
Milos <epicmilos@gmail.com>
2018-12-25 15:03:02 +0100

    reedit 5

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser'); //takes JSON and converts it into object
-var ObjectID = require('mongodb');
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
